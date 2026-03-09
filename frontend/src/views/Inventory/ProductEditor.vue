890587819451db36c3909ffae7678330518c5575
User <user@example.com>
2026-03-09 18:51:25 +0200

    style: fix double scrollbar on product editor page

--- frontend/src/views/Inventory/ProductEditor.vue
+++ frontend/src/views/Inventory/ProductEditor.vue
@@ -322,10 +322,15 @@ onMounted(() => {
 
 <style scoped>
 .page-container {
-  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
   flex-direction: column;
   background-color: #f4f6f8;
+  overflow: hidden;
 }
 
 /* === TOP BAR === */
